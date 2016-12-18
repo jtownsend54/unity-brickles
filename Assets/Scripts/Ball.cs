84c05c64306721ae6d11f454b89f18cadf6ede04
Jon Townsend <jon.townsend@limelyte.com>
2016-12-18 12:01:21 -0800

    Updates
    Added paddle movement with mouse
    Ball is sticky to paddle until click happens
    Add velocity to ball when click happens
    Added invisible colliders around the boundries

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private bool isStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isStarted) {
+			return;
+		}
+
+		this.transform.position = new Vector3(paddle.transform.position.x, this.transform.position.y, 0);
+
+		if(Input.GetMouseButtonDown(0)) {
+			isStarted = true;
+			this.rigidbody2D.velocity = new Vector2(2f, 10f);
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	private float mousePosX;
+	private Vector3 mousePos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosX = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
+		mousePos = new Vector3(mousePosX, this.transform.position.y, 0f);
+
+		this.transform.position = mousePos;
+	}
+}
