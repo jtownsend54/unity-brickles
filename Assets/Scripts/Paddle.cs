b46d0f276265abd84948ba77f7cf9a48ff68324e
Jon Townsend <jon.townsend@limelyte.com>
2017-01-07 00:02:29 -0800

    Added new background for level 2
    Also added option for play testing. If this property on the paddle is true, it will follow the ball automatically.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,19 +2,34 @@
 using System.Collections;
 
 public class Paddle : MonoBehaviour {
+	public bool autoPlay = false;
+
 	private float mousePosX;
 	private Vector3 mousePos;
+	private Ball ball;
 
-	// Use this for initialization
 	void Start () {
-	
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		mousePosX = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 1.5f, 14.5f);
-		mousePos = new Vector3(mousePosX, this.transform.position.y, 0f);
 
+	// Update is called once per frame
+	// Mouse Position and Ball position are not measured in the same units, 
+	// which is why the mouse position needs to be converted into game units
+	void Update () {
+		float xPos = Input.mousePosition.x / Screen.width * 16;
+
+		// Automatically move paddle with ball
+		if(autoPlay) {
+			xPos = ball.transform.position.x;
+		}
+
+		UpdatePaddlePos(xPos);
+	}
+
+	void UpdatePaddlePos(float xPos) {
+		mousePosX = Mathf.Clamp(xPos, 1.5f, 14.5f);
+		mousePos = new Vector3(mousePosX, this.transform.position.y, 0f);
+		
 		this.transform.position = mousePos;
 	}
 }
