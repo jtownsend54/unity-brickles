72826ce5dfa25a8e0c3f41611dcb33bb0b4b1e27
Jon Townsend <jon.townsend@limelyte.com>
2017-01-14 00:08:10 -0800

    Added smoke puff particle system when a block is destroyed.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,6 +5,7 @@ public class Brick : MonoBehaviour {
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0;
 	public AudioClip crack;
+	public GameObject smoke;
 
 	private int hits;
 	private LevelManager manager;
@@ -15,6 +16,8 @@ public class Brick : MonoBehaviour {
 		hits 	= 0;
 		manager = GameObject.FindObjectOfType<LevelManager>();
 		isBreakable = (this.tag == "unbreakable");
+		//smoke.transform.position.y = this.transform.position.y;
+		//smoke.transform.position.x = this.transform.position.x;
 		breakableCount++;
 	}
 	
@@ -33,12 +36,19 @@ public class Brick : MonoBehaviour {
 		}
 
 		if (hits >= hitSprites.Length + 1) {
+			smoke.particleSystem.startColor = this.GetComponent<SpriteRenderer>().color;
+			Instantiate(smoke, gameObject.transform.position, Quaternion.identity);
 			breakableCount--;
 			Destroy(gameObject);
+
 			manager.BrickDestroyed();
 		} else {
 			this.GetComponent<SpriteRenderer>().sprite = hitSprites[hits - 1];
 		}
+	}
+
+	void CreateSmoke() {
+
 	}
 
 	void SimulateWin() {
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string name) {
 		Debug.Log("Load the level: " + name);
+		Brick.breakableCount = 0;
 		Application.LoadLevel (name);
 	}
 
 	public void LoadNextLevel() {
+		Brick.breakableCount = 0;
 		Application.LoadLevel(Application.loadedLevel + 1);
 	}
 
